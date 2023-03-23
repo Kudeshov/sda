86a6e63ce353788b50c6941e5217ed4bfc4d6d71
Evgeny Kudeshov <eug@postmodern.ru>
2023-03-23 17:17:09 +0200

    common_queries.js

---
+++ Back/common_queries.js
@@ -0,0 +1,49 @@
+const getNLSQuery = (name, descr, table_id, lang_id, table_name) => 
+{
+  var descr_field_name = 'descr';
+  if (table_name==='dose_ratio') descr_field_name = 'fullname';
+
+  const sql_nls = `DO $$ `+
+  `declare aname VARCHAR(50) := '${name}'; `+
+  `declare adesc text := '${descr}'; `+
+  `declare a${table_name}_id INT4 := ${table_id}; `+
+  `declare alang_id INT4 := ${lang_id}; `+
+  `BEGIN `+
+  `IF (( aname = '' ) and (adesc = '') ) THEN `+ //если пустое, удаляем
+  `    begin DELETE FROM nucl.${table_name}_nls WHERE ${table_name}_id = ${table_id} and lang_id = alang_id; end; `+
+  `ELSE `+
+  `     begin `+ //если не пустое, добавляем или апдейтим
+  `       INSERT INTO nucl.${table_name}_nls (name, ${descr_field_name}, ${table_name}_id, lang_id) VALUES (aname, adesc, a${table_name}_id, alang_id) `+ 
+  `       ON CONFLICT (${table_name}_id, lang_id) do UPDATE SET name = aname, ${descr_field_name}=adesc; `+ 
+  `     end; `+
+  `END IF; `+
+  `END; `+
+  `$$ `;
+  return sql_nls;
+}
+
+const getNLSQueryNoDescr = (name, table_id, lang_id, table_name) => 
+{
+  const sql_nls = `DO $$ `+
+  `declare aname VARCHAR(50) := '${name}'; `+
+  `declare a${table_name}_id INT4 := ${table_id}; `+
+  `declare alang_id INT4 := ${lang_id}; `+
+  `BEGIN `+
+  `IF ( aname = '' )  THEN `+ //если пустое, удаляем
+  `    begin DELETE FROM nucl.${table_name}_nls WHERE ${table_name}_id = ${table_id} and lang_id = alang_id; end; `+
+  `ELSE `+
+  `     begin `+ //если не пустое, добавляем или апдейтим
+  `       INSERT INTO nucl.${table_name}_nls (name, ${table_name}_id, lang_id) VALUES (aname, a${table_name}_id, alang_id) `+ 
+  `       ON CONFLICT (${table_name}_id, lang_id) do UPDATE SET name = aname; `+ 
+  `     end; `+
+  `END IF; `+
+  `END; `+
+  `$$ `;
+  return sql_nls;
+}
+
+module.exports = {
+    getNLSQuery,
+    getNLSQueryNoDescr
+  }
+  
